45bced5a9b3d977a4e5647623247bed10eff7aad
Aniket <janiket338@gmail.com>
2020-12-19 19:31:59 +0530

    Added Blog project

---
+++ blog_project/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(
+        default=timezone.now)    # auto_now, auto_now_add
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post-detail', kwargs={'pk': self.pk})
